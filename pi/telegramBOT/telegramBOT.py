a1080d34087b91a76b619b8dca33af85a51756ec
julianes10 <julianes10@yahoo.com>
2019-07-02 23:54:11 +0200

    Draft apirest to control async behaviour of telegrambot

--- pi/telegramBOT/telegramBOT.py
+++ pi/telegramBOT/telegramBOT.py
@@ -16,19 +16,105 @@ import random
 import shutil
 import telebot
 from helper import *
+from flask import Flask, render_template,redirect
+from flask import Flask, jsonify,abort,make_response,request, url_for
 
 _FINISHTASKS=False
+
+
+'''----------------------------------------------------------'''
+'''----------------      API REST         -------------------'''
+'''----------------------------------------------------------'''
+api = Flask("api",template_folder="templates",static_folder='static')
+
+
+'''----------------------------------------------------------'''
+@api.route('/api/v1.0/telegramBOT/action', methods=['POST'])
+def post_telegram_action():
+    if not request.json:
+        helper.internalLogger.debug("It is not a json. Back with error")
+        abort(400)
+    return requestNewAction(request.json)
+
+
+def requestNewAction(req):
+    rt=jsonify({'result': 'OK'})
+
+    if not "action" in req:
+        rt=jsonify({'result': 'KO'})
+    else:
+      helper.internalLogger.debug("NEW timelapse is requested. Autocanceling current one if any")
+      if "name" in ongoing:
+        cleanUpOngoing(ongoing["name"],True,True)
+        helper.internalLogger.debug("DELETING existing ones with same name if any")
+        purgeProject(ongoing["name"])
+      ongoing.clear()
+      ongoing["name"]=req["name"]
+      ongoing["interval"]=int(req["interval"])
+      if "maxTime" in req:
+       if req["maxTime"] != "": 
+        ongoing["maxTime"]=int(req["maxTime"])
+      if "maxNbrOfPictures" in req:
+       if req["maxNbrOfPictures"] != "":
+        ongoing["maxNbrOfPictures"]=int(req["maxNbrOfPictures"])
+      ongoing["status"]="ONGOING"
+      cleanUpOngoing(ongoing["name"],True,True)
+    UNLOCK(ongoing) 
+    return rt
+
+'''----------------------------------------------------------'''
+@api.route('/',methods=["GET", "POST"])
+@api.route('/telegramBOT/',methods=["GET", "POST"])
+def telegramBOT_home():
+    if request.method == 'POST':
+      helper.internalLogger.debug("Processing new request from a form...{0}".format(request.form))
+      form2 = request.form.to_dict()
+      helper.internalLogger.debug("Processing new request from a form2...{0}".format(form2))   
+      requestNewAction(form2)
+    
+    url={}
+
+    st={} # getStatus()
+    rt=render_template('index.html', title="TelegramBOT Site",status=st)
+    return rt
+
+'''----------------------------------------------------------'''
+@api.route('/api/v1.0/telegramBOT/status', methods=['GET'])
+def get_telegramBOT_status():
+    return json.dumps(getStatus())
+
+def getStatus():
+    ongoing=LOCK()
+    UNLOCK(ongoing)
+    return rt
 
 '''----------------------------------------------------------'''
 '''----------------       periodic tasks  -------------------'''
 '''----------------------------------------------------------'''
 
 def periodicTasks():
-  #TODO custom periodict task and make sleep period smaller, interval by idlist
+
   global bot
   global chatidList
   helper.internalLogger.debug("TeleBot periodic start")
   while not _FINISHTASKS:
+
+   for key,item in GLB_configuration["actions"].items():
+          if key in GLB_configuration["menu"]:
+            helper.internalLogger.debug("Checking key '{0}' and msg {1}".format(key,msg))
+            if msg == key.lower():
+              helper.internalLogger.debug("Command '{0}' executed".format(key))
+              result=runAction(item,message.text)  
+              if result is None:
+                   bot.send_message(message.chat.id,'Error: Exception executing {0}'.format(key))
+              else:
+                  sendActionResult(message.chat.id,item,result)    
+              return     
+        bot.reply_to(message, "Ignoring this request.")
+    time.sleep(1)
+
+
+    ## iterate tasks, run commands if applied and send to peers subscribed.
     helper.internalLogger.debug("TeleBot periodic")
     if len(chatidList)>0:
       for i in chatidList:
@@ -40,21 +126,33 @@ def periodicTasks():
 '''----------------------------------------------------------'''
 '''----------------    runAction            -----------------'''
 
-def runAction(chatid,action,originalMsg):
+def runAction(action,originalMsg):
+  rt=None
   try:
+
     cmd=action["cmd"]
+    if "TELEGRAM_COMMAND" in action["cmd"]:
+        helper.internalLogger.debug("Custom cmd with TELEGRAM_COMMAND {0}'".format(cmd)) 
+        msgSplitted=originalMsg.split()     
+        cmd=action["cmd"].replace("TELEGRAM_COMMAND",' '.join(msgSplitted[1:]))
+  
     global bot
     result=subprocess.check_output(['bash','-c',cmd])
     helper.internalLogger.debug("Cmd {0}, result {1}".format(cmd,result))
+    rt=result
   except Exception as e:
     e = sys.exc_info()[0]
     helper.internalLogger.critical('Error: Exception executing {0}'.format(action))
     helper.einternalLogger.exception(e)  
-    bot.send_message(chatid,'Error: Exception executing {0}'.format(action))
-    return
-
+ 
   helper.internalLogger.debug("Let's respond with associated info to action{0}".format(action))
+  return rt
 
+
+'''----------------------------------------------------------'''
+'''----------------    sendActionResult     -----------------'''
+
+def sendActionResult(chatid,action,result):
   #Coding up to 5 retrials
   retry=0
   while retry < 5:
@@ -112,6 +210,41 @@ def runAction(chatid,action,originalMsg):
   ### TODO limpiar los ficheros??? no se, se carga multiusuario...
 
   return None
+
+
+'''----------------------------------------------------------'''
+def LOCK():
+  global GLB_configuration 
+  rt = {}
+  if not "ongoingDB" in GLB_configuration:
+    return rt
+
+  p=GLB_configuration["ongoingDB"]+".lock"
+  while os.path.exists(p):
+    time.sleep(1)
+  # create the lock file
+  lock_file = open(p, "w")
+  lock_file.close()
+
+
+  try:
+      with open(GLB_configuration["ongoingDB"]) as json_data:
+          rt = json.load(json_data)
+  except Exception as e:
+      helper.internalLogger.critical("Error opening ongoingDB: {0}.".format(GLB_configuration["ongoingDB"]))
+      helper.einternalLogger.exception(e)
+  
+  return rt
+
+'''----------------------------------------------------------'''
+def UNLOCK(data):
+  with open(GLB_configuration["ongoingDB"], 'w') as fp:
+    json.dump(data, fp)
+
+  p=GLB_configuration["ongoingDB"]+".lock"
+  if os.path.exists(p):
+    os.remove(p)
+
 '''----------------------------------------------------------'''
 '''----------------       M A I N         -------------------'''
 '''----------------------------------------------------------'''
@@ -140,11 +273,20 @@ def main(configfile):
   print('See logs debugs in: {0} and exeptions in: {1}-----------'.format(cfg_log_debugs,cfg_log_exceptions))  
   helper.internalLogger.critical('telegramBOT-start -------------------------------')  
   helper.einternalLogger.critical('telegramBOT-start -------------------------------')
-
+  UNLOCK({}) # cleanup
 
 
   try:    
     logging.getLogger("requests").setLevel(logging.DEBUG)
+
+    helper.internalLogger.debug("Starting restapi...")
+ 
+    apiRestTask=threading.Thread(target=apirest_task,name="restapi")
+    apiRestTask.daemon = True
+    apiRestTask.start()
+
+
+
     helper.internalLogger.debug("Calling telepot.Bot...")
     global bot
     global chatidList
@@ -176,7 +318,11 @@ def main(configfile):
             helper.internalLogger.debug("Checking key '{0}' and msg {1}".format(key,msg))
             if msg == key.lower():
               helper.internalLogger.debug("Command '{0}' executed".format(key))
-              runAction(message.chat.id,item,message.text)    
+              result=runAction(item,message.text)  
+              if result is None:
+                   bot.send_message(message.chat.id,'Error: Exception executing {0}'.format(key))
+              else:
+                  sendActionResult(message.chat.id,item,result)    
               return     
         bot.reply_to(message, "Ignoring this request.")
 
@@ -203,6 +349,13 @@ def main(configfile):
     if not pt==None:
       pt.join()
     loggingEnd()
+
+
+'''----------------------------------------------------------'''
+'''----------------     apirest_task      -------------------'''
+def apirest_task():
+
+  api.run(debug=True, use_reloader=False,port=GLB_configuration["port"],host=GLB_configuration["host"])
 
 
 '''----------------------------------------------------------'''
