07dd28a68135f89f77c11713ce9a848704515df2
julianes10 <julianes10@yahoo.com>
2020-02-26 14:30:46 +0100

    new gpio on off dummy cmdline wrapper for pi

---
+++ pi/gpio/gpio.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import argparse
+
+'''----------------------------------------------------------'''
+'''----------------       M A I N         -------------------'''
+'''----------------------------------------------------------'''
+
+def main(arg):
+  import RPi.GPIO as GPIO
+
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
+
+  GPIO.setup(arg.pin, GPIO.OUT) # GPIO Assign mode
+  if arg.value==0:
+    GPIO.output(arg.pin, GPIO.LOW) # out
+  else:
+    GPIO.output(arg.pin, GPIO.HIGH) # out
+
+
+'''----------------------------------------------------------'''
+'''----------------     parse_args        -------------------'''
+def parse_args():
+    """Parse the args."""
+    parser = argparse.ArgumentParser(
+        description='GPIO wrapper')
+    parser.add_argument('--pin', type=int, required=True,
+                        help='GPIO number')
+    parser.add_argument('--value', type=int, required=True,
+                        help='1 or 0')
+    return parser.parse_args()
+
+'''----------------------------------------------------------'''
+'''----------------       '__main__'      -------------------'''
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
+
+
+    
+
+
