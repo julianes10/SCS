7f49289e3392116c8ecde4a066fad0428f73af2b
julianes10 <julianes10@yahoo.com>
2020-09-15 19:25:29 +0200

    new arduino  wrapper sensor for HCSR04

---
+++ arduino/HCSR04sensor/HCSR04sensor.cpp
@@ -0,0 +1,50 @@
+/* BASED ON https://www.instructables.com/id/Non-blocking-Ultrasonic-Sensor-for-Arduino */
+#include "HCSR04sensor.h"
+
+
+HCSR04sensor *HCSR04sensor::_instance(NULL);
+
+HCSR04sensor::HCSR04sensor(int trigger, int echo, int interrupt, int max_dist)
+    : _trigger(trigger), _echo(echo), _int(interrupt), _max(max_dist), _finished(false)
+{
+  if(_instance==0) _instance=this;   
+  _latestRead=0; 
+}
+
+void HCSR04sensor::setup(){
+  pinMode(_trigger, OUTPUT);
+  digitalWrite(_trigger, LOW);
+  pinMode(_echo, INPUT);  
+  attachInterrupt(_int, _echo_isr, CHANGE);
+}
+
+void HCSR04sensor::trigger(){
+  _finished=false;
+  digitalWrite(_trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(_trigger, LOW);  
+}
+
+unsigned int HCSR04sensor::getOngoingDistance(){
+  unsigned int rt=0;
+  if (_finished){
+    _latestRead=(_end-_start)/(58);
+    rt= _latestRead;
+  }
+  return rt;
+}
+
+void HCSR04sensor::_echo_isr(){
+  HCSR04sensor* _this=HCSR04sensor::instance();
+  
+  switch(digitalRead(_this->_echo)){
+    case HIGH:
+      _this->_start=micros();
+      break;
+    case LOW:
+      _this->_end=micros();
+      _this->_finished=true;
+      break;
+  }   
+}
+
---
+++ arduino/HCSR04sensor/HCSR04sensor.h
@@ -0,0 +1,28 @@
+/* BASED ON https://www.instructables.com/id/Non-blocking-Ultrasonic-Sensor-for-Arduino */
+#ifndef HCSR04sensor_H
+#define HCSR04sensor_H
+
+#include <Arduino.h>
+
+
+class HCSR04sensor {
+  public:
+    HCSR04sensor(int trigger, int echo, int interrupt, int max_dist=200);   
+    void setup();
+    void trigger();
+    bool isFinished(){ return _finished; }
+    unsigned int getOngoingDistance();
+    unsigned int getLatestDistanceRead() {return _latestRead; }
+    static HCSR04sensor* instance(){ return _instance; }
+    
+  private:
+    static void _echo_isr();
+    
+    int _trigger, _echo, _int, _max;
+    int _latestRead;
+    volatile unsigned long _start, _end;
+    volatile bool _finished;
+    static HCSR04sensor* _instance;
+};
+
+#endif
